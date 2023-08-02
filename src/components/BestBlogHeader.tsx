37d4a9a9724c8326ad6170cbd98d3336aeacb162
Amir <amir.sahit@gmail.com>
2023-08-02 17:58:04 +0200

    unified main buttons

--- src/components/BestBlogHeader.tsx
+++ src/components/BestBlogHeader.tsx
@@ -4,10 +4,7 @@ function BestBlogHeader({ createPost, setCreatePost }: CreatePostProps) {
   return (
     <header className="w-full min-w-[200px] bg-light-blue p-5 text-center sm:w-[20vw] sm:rounded-subRounded">
       <h1 className="text-3xl font-bold">BestBlog</h1>
-      <button
-        onClick={() => setCreatePost(!createPost)}
-        className="mt-5 w-[80vw] rounded-subRounded bg-white px-4 py-2 text-lg font-bold text-light-blue sm:w-full"
-      >
+      <button onClick={() => setCreatePost(!createPost)} className="mainButton">
         {!createPost ? "Create Your Post" : "Close"}
       </button>
     </header>
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -9,16 +9,12 @@ function DisplaySinglePost() {
 
   return (
     <main className="flex h-[100vh] flex-col items-start justify-around bg-light-blue sm:flex-row sm:bg-white sm:p-5">
-      <header className="flex w-full min-w-[200px] flex-col justify-center bg-light-blue p-5 text-center sm:w-[20vw] sm:rounded-subRounded">
+      <header className="flex w-full min-w-[200px] flex-col items-center bg-light-blue p-5 text-center sm:w-[20vw] sm:rounded-subRounded">
         <h1 className="text-3xl font-bold">BestBlog</h1>
-        <Link
-          href="/"
-          className="mt-5 w-[80vw] self-center rounded-subRounded bg-white px-4 py-2 text-lg font-bold text-light-blue sm:w-full"
-        >
+        <Link href="/" className="mainButton">
           Back
         </Link>
       </header>
-
       <section className="flex h-full w-full flex-col items-center bg-light-blue p-5 sm:h-full sm:max-h-[90vh] sm:max-w-[60vw] sm:justify-between sm:rounded-mainRounded">
         <article className="flex h-auto max-h-[70vh] w-full flex-col gap-3 overflow-scroll rounded-subRounded bg-white p-5 sm:max-h-full">
           <p className="text-end">
